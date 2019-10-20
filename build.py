401c1d5252492faf4e27182fdfc53cc4ee255d93
Casper da Costa-Luis <casper.dcl@physics.org>
2019-10-21 00:13:55 +0100

    initial scraping concept

---
+++ build.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+from __future__ import print_function, division
+import os
+import sys
+import base64
+
+import requests
+from tqdm import tqdm
+from github import Github
+import yaml
+
+g = Github(os.getenv("GITHUB_API_TOKEN"))
+repos = list(g.get_organization("OpenWorm").get_repos())
+
+
+def repo2content(repo):
+    """
+    @return {"filename": "content"} where `filename` starts with `.openworm.`
+    """
+    tree = repo.get_git_tree("master", recursive=False).tree
+    files = [i for i in tree if i.path.lower().startswith('.openworm.')]
+    if not files:
+        return {}
+    reqs = [requests.get(i.url) for i in files]
+    data = [
+        base64.decodestring(
+            requests.get(i.url).json()['content']
+        ) for i in reqs
+    ]
+    return dict(zip((i.path.lower() for i in files), data))
+
+
+def repo2meta(repo):
+    content = repo2content(repo)
+    if not content:
+        return {}
+    meta = {}
+    for file, data in content.items():
+        if file.endswith('.yml') or file.endswith('.yaml'):
+            meta.update(yaml.load(data))
+        elif file.endswith('.rst'):
+            raise NotImplementedError
+        elif file.endswith('.md'):
+            raise NotImplementedError
+        else:
+            raise KeyError("Unknown file extension: {}".format(file))
+    return meta
+
+
+def main():
+    meta = {}
+
+    for repo in tqdm(repos, unit="repo"):
+        files = repo2meta(repo)
+        if files:
+            meta[repo.name] = files
+    print(meta)
+
+
+if __name__ == "__main__":
+    sys.exit(main() or 0)
